d8f77afa62dbe3ad0088d37c0db5f3e34fd358dd
Psyk0loge <SimonGoetz99@gmx.de>
2025-04-18 11:33:27 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+//Todo: check if I need this
+app.UseAuthorization();
+app.UseRouting();
+
+app.UseAntiforgery();
+
+app.Run();
